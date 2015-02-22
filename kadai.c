81c8a5cf5f8fa5f19f727e7bae11b04bfc56af0c
sh4869 <nobuk4869@gmail.com>
2015-02-23 04:38:57 +0900

    Fix Graphs Function

--- kadai.c
+++ kadai.c
@@ -13,173 +13,206 @@
 float cast(char *equ,int length);
 int main(int argc,char **argv)
 {
-  Display *dpy;
-  Window w,quit,input,buttons[16],draw,clear;
-  Window root;
-  int    screen,xPoint,yPoint,l = 0,count = 0,yoko = 0,tate = 0,equCount = 0,equFlag = 0,value,equLength;
-  float coef = 0;
-  int coefX,coefY;
-  char formula[255],string[255];
-  char buttonString[16] = {'1','2','3','+','4','5','6','-','7','8','9','*','0','x','=','/'};
-  char equ[255];
-  char text[100];
-  unsigned long black, white;
-  GC       gc;
-  KeySym key;
-  XColor c0[2],c1[2];
-  XEvent e;
-  Colormap cmap;
+	Display *dpy;
+	Window w,quit,input,buttons[16],draw,clear;
+	Window root;
+	int    screen,xPoint,yPoint,l = 0,count = 0,yoko = 0,tate = 0,equCount = 0,equFlag = 0,value,equLength;
+	float coef[10];
+	int coefCount = 0;
+	int mustOpe = 0,forceDraw = 0;
+	int coefX,coefY;
+	char formula[255],string[255];
+	char buttonString[16] = {'1','2','3','+','4','5','6','-','7','8','9','*','0','x','=','/'};
+	char equ[255];
+	char text[100];
+	unsigned long black, white;
+	GC       gc;
+	KeySym key;
+	XColor c0[2],c1[2];
+	XEvent e;
+	Colormap cmap;
 
-  dpy = XOpenDisplay("");
-  root = DefaultRootWindow (dpy);
-  screen = DefaultScreen (dpy);
-  white = WhitePixel (dpy, screen);
-  black = BlackPixel (dpy, screen);
-  cmap = DefaultColormap(dpy,0);
-  XAllocNamedColor(dpy,cmap,"red",&c1[0],&c0[0]);
-  XAllocNamedColor(dpy,cmap,"blue",&c1[1],&c0[1]);
+	dpy = XOpenDisplay("");
+	root = DefaultRootWindow (dpy);
+	screen = DefaultScreen (dpy);
+	white = WhitePixel (dpy, screen);
+	black = BlackPixel (dpy, screen);
+	cmap = DefaultColormap(dpy,0);
+	XAllocNamedColor(dpy,cmap,"red",&c1[0],&c0[0]);
+	XAllocNamedColor(dpy,cmap,"blue",&c1[1],&c0[1]);
 
-  
-  /*------------- Window Setting -------------------*/
 
-  w = XCreateSimpleWindow(dpy, root, 100, 100, WIDTH, HIGHT, BORDER, black, white);
-  quit = XCreateSimpleWindow(dpy,w,10,3,30,12,BORDER,black,white);
-  input = XCreateSimpleWindow(dpy,w,510,10,260,480,BORDER,black,white);
-  draw = XCreateSimpleWindow(dpy,w,520,30,40,20,BORDER,black,white);
-  clear = XCreateSimpleWindow(dpy,w,580,30,40,20,BORDER,black,white);
-  for(count = 0;count < 16;count++){
-	buttons[count] = XCreateSimpleWindow(dpy,w,550+tate*50,200+yoko*80,30,30,BORDER,black,white);
-	tate++;
-	if((count+1) % 4 == 0){
-	  yoko++;
-	  tate = 0;
+	/*------------- Window Setting -------------------*/
+
+	w = XCreateSimpleWindow(dpy, root, 100, 100, WIDTH, HIGHT, BORDER, black, white);
+	quit = XCreateSimpleWindow(dpy,w,10,3,30,12,BORDER,black,white);
+	input = XCreateSimpleWindow(dpy,w,510,10,260,480,BORDER,black,white);
+	draw = XCreateSimpleWindow(dpy,w,520,30,40,20,BORDER,black,white);
+	clear = XCreateSimpleWindow(dpy,w,580,30,40,20,BORDER,black,white);
+	for(count = 0;count < 16;count++){
+		buttons[count] = XCreateSimpleWindow(dpy,w,550+tate*50,200+yoko*80,30,30,BORDER,black,white);
+		tate++;
+		if((count+1) % 4 == 0){
+			yoko++;
+			tate = 0;
+		}
 	}
-  }
 
-  gc = XCreateGC(dpy, w, 0, NULL);
+	gc = XCreateGC(dpy, w, 0, NULL);
 
-  /* -------  Select Input --------------*/
-  XSelectInput(dpy,w,ButtonPressMask | ButtonReleaseMask | KeyPressMask);
-  XSelectInput(dpy,quit,ButtonPressMask);
-  XSelectInput(dpy,input,ButtonPressMask);
-  XSelectInput(dpy,draw,ButtonPressMask);
-  XSelectInput(dpy,clear,ButtonPressMask);
-  for(count = 0;count < 16;count++){
-  	XSelectInput(dpy,buttons[count],ButtonPressMask);
-  }
+	/* -------  Select Input --------------*/
+	XSelectInput(dpy,w,ButtonPressMask | ButtonReleaseMask | KeyPressMask);
+	XSelectInput(dpy,quit,ButtonPressMask);
+	XSelectInput(dpy,input,ButtonPressMask);
+	XSelectInput(dpy,draw,ButtonPressMask);
+	XSelectInput(dpy,clear,ButtonPressMask);
+	for(count = 0;count < 16;count++){
+		XSelectInput(dpy,buttons[count],ButtonPressMask);
+	}
 
-  XMapWindow(dpy, w);
-  XMapSubwindows(dpy,w);
-  while(1){
-	//Event
-	XNextEvent(dpy,&e);
-	switch (e.type){
-	  case KeyPress:
-		printf("keycode=%d \n",e.xkey.keycode);
-		if(e.xkey.keycode == 24) return 0;
-		break;
-
-	  case ButtonPress :
-		printf("x=%d y=%d button=%d \n",e.xbutton.x,e.xbutton.y,e.xbutton.button);
-		
-		if(e.xany.window == quit){ 
-		  printf("Exit!\n");
-		  return 0;
-		}
-		if(e.xany.window == clear){
-			printf("Clear!\n");
-			XClearWindow(dpy,w);
-			XClearWindow(dpy,input);
-			l = 0;
-			equCount = 0;
-			equLength = strlen(equ);
-			memset(equ,'\0',equLength);
-		}
-		if(e.xany.window == draw){
-			printf("Draw!\n");
-			switch(equFlag){
-				case 0:
-			    	value = atoi(equ);
+	for(count = 0;count < 10;count++) coef[count] = 0;
+	count = 0;
+	XMapWindow(dpy, w);
+	XMapSubwindows(dpy,w);
+	while(1){
+		//Event
+		XNextEvent(dpy,&e);
+		switch (e.type){
+			case KeyPress:
+				printf("keycode=%d \n",e.xkey.keycode);
+				if(e.xkey.keycode == 24) return 0;
+				if(e.xkey.keycode == 54){
+					printf("Clear!\n");
+					XClearWindow(dpy,w);
+					XClearWindow(dpy,input);
+					l = 0;
+					equCount = 0;
+					equLength = strlen(equ);
 					memset(equ,'\0',equLength);
-					break;
-				case 1:
-					value = cast(equ,strlen(equ));
-					break;
-				case 2:
-					printf("Please wait I develop\n");
-					break;
-				default:
-					break;
-			}
-			l = 0;
-			equCount = 0;
-			equLength = strlen(equ);
-			memset(equ,'\0',equLength);
-			XClearWindow(dpy,input);
-			if(equFlag == 0 || equFlag == 1){
-				XDrawLine(dpy,w,gc,10,250-value,490,250-value);
-				XDrawString(dpy,input,gc,160,140,equ,strlen(equ));
-			}
+				    XDrawString(dpy,w,gc,491,250,"X",1);
+					XDrawString(dpy,w,gc,250,8,"Y",1);
+				    XDrawString(dpy,input,gc,15,80,"Y =",3);
+					XDrawString(dpy,input,gc,70,140,"Last Answer:",12);
+					XDrawLine(dpy,w,gc,10,250,490,250);
+					XDrawLine(dpy,w,gc,250,10,250,490);
+				}
+				break;
 
+			case ButtonPress :
+				printf("x=%d y=%d button=%d \n",e.xbutton.x,e.xbutton.y,e.xbutton.button);
+
+				while(count < 16){
+					if(e.xany.window == buttons[count]){
+						if(mustOpe == 1 && (count+1) % 4 != 0){
+							forceDraw = 1;
+							printf("Force!\n");
+							break;
+						}else{
+							mustOpe = 0;
+						}
+						XDrawString(dpy,input,gc,35+l,80,&buttonString[count],1);
+						l += 10;
+						equ[equCount] = buttonString[count];
+						equCount++;
+						if((count+1) % 4 == 0){
+							equFlag = 1;
+						}
+						if(count == 13){
+							coef[coefCount] = cast(equ,strlen(equ));
+							coefCount++;
+							equFlag = 2;
+							mustOpe = 1;
+						}
+					}
+					count++;
+				}
+
+				if(e.xany.window == quit){ 
+					printf("Exit!\n");
+					return 0;
+				}
+				if(e.xany.window == clear){
+					printf("Clear!\n");
+					XClearWindow(dpy,w);
+					XClearWindow(dpy,input);
+					l = 0;
+					equCount = 0;
+					equLength = strlen(equ);
+					memset(equ,'\0',equLength);
+				}
+				if(e.xany.window == draw || forceDraw == 1){
+					printf("Draw!\n");
+					switch(equFlag){
+						case 0:
+							value = atoi(equ);
+							XClearWindow(dpy,input);
+							XDrawLine(dpy,w,gc,10,250-value,490,250-value);
+							XDrawString(dpy,input,gc,160,140,equ,strlen(equ));
+							break;
+						case 1:
+							value = cast(equ,strlen(equ));
+							XClearWindow(dpy,input);
+							XDrawLine(dpy,w,gc,10,250-value,490,250-value);
+							XDrawString(dpy,input,gc,160,140,equ,strlen(equ));
+							break;
+						case 2:
+							XClearWindow(dpy,input);
+							sprintf(text,"%fx",coef[coefCount - 1]);
+							XDrawString(dpy,input,gc,160,140,text,strlen(text));
+							for(count = 0;count < 480;count++){
+		  						coefX = count + 10;
+		  						coefY = -((count-240) * coef[coefCount - 1]) + 250;
+		  						XDrawPoint(dpy,w,gc,coefX,coefY);
+		  						coefX = coefY = 0;
+		  					}
+		  					forceDraw = mustOpe = 0;
+		  					for(count = 0;count < 10;count++) coef[count] = 0;
+							break;
+						default:
+							break;
+					}
+					equFlag = 0;
+					l = 0;
+					equCount = 0;
+					equLength = strlen(equ);
+					memset(equ,'\0',equLength);
+					if(equFlag == 2){
+						sprintf(text,"%fx",coef[coefCount - 1]);
+						XDrawString(dpy,input,gc,160,140,text,strlen(text));
+					}
+				}
+
+				/*----------- Basic Drawing -------------*/
+
+				XDrawString(dpy,quit,gc,4,10,"Exit",4);
+				XDrawString(dpy,draw,gc,4,10,"Draw!",5);
+				XDrawString(dpy,clear,gc,4,10,"Clear!",6);
+				XDrawString(dpy,w,gc,491,250,"X",1);
+				XDrawString(dpy,w,gc,250,8,"Y",1);
+				XDrawString(dpy,input,gc,15,80,"Y =",3);
+				XDrawString(dpy,input,gc,70,140,"Last Answer:",12);
+				XDrawLine(dpy,w,gc,10,250,490,250);
+				XDrawLine(dpy,w,gc,250,10,250,490);
+				tate = yoko = 0;
+				for(count = 0;count < 16;count++){
+					XDrawString(dpy,buttons[count],gc,13,20,&buttonString[count],1);
+					tate++;
+					if((count+1) % 4 == 0){
+						yoko++;
+						tate = 0;
+					}
+				}
+				count = forceDraw = 0;
+				/*-----------------------------------------*/
+				break;
 		}
-		
-		while(count < 16){
-		  if(e.xany.window == buttons[count]){
-		  	if(count != 13){
-		  		XDrawString(dpy,input,gc,35+l,80,&buttonString[count],1);
-		  		l += 10;
-		  		equ[equCount] = buttonString[count];
-		  		equCount++;
-		  		if((count+1) % 4 == 0){
-		  			equFlag = 1;
-		  		}
-		 	}else{
-		  		coef = cast(equ,strlen(equ));
-		  		l = 0;
-				equCount = 0;
-				equLength = strlen(equ);
-				memset(equ,'\0',equLength);
-				XClearWindow(dpy,input);
-				sprintf(text,"%fx",coef);
-				XDrawString(dpy,input,gc,160,140,text,strlen(text));
-		  		for(count = 0;count < 480;count++){
-		  			coefX = count + 10;
-		  			coefY = -((count-240) * coef) + 250;
-		  			XDrawPoint(dpy,w,gc,coefX,coefY);
-		  			coefX = coefY = 0;
-		  		}
-		  	}
-		   }
-		   count++;
-		}
-		
-		
-		/*----------- Basic Drawing -------------*/
-		
-		XDrawString(dpy,quit,gc,4,10,"Exit",4);
-		XDrawString(dpy,draw,gc,4,10,"Draw!",5);
-		XDrawString(dpy,clear,gc,4,10,"Clear!",6);
-		XDrawString(dpy,w,gc,491,250,"X",1);
-		XDrawString(dpy,w,gc,250,8,"Y",1);
-		XDrawString(dpy,input,gc,15,80,"Y =",3);
-		XDrawString(dpy,input,gc,70,140,"Last Answer:",12);
-		XDrawLine(dpy,w,gc,10,250,490,250);
-		XDrawLine(dpy,w,gc,250,10,250,490);
-		tate = yoko = 0;
-		for(count = 0;count < 16;count++){
-		  XDrawString(dpy,buttons[count],gc,13,20,&buttonString[count],1);
-	      tate++;
-	      if((count+1) % 4 == 0){
-	        yoko++;
-	  		tate = 0;
-		  }
-	    }
-	    count = 0;
-	    /*-----------------------------------------*/
-		break;
 	}
-  }
 }
+
+/*
+   @opeFlag 0:計算しなくていい 1:計算する
+   @ope   0: +   1: -   2: *   3: /      
+   										*/
 
 float cast(char *equ,int length){
 	int opeFlag = 0;
@@ -187,12 +220,14 @@ float cast(char *equ,int length){
 	float result = 0;
 	int values[10];
 	char valuesChar[10];
+	// 長さの文だけequの中身を見ていく
 	while(count < length){
 		switch(equ[count]){
-			case '+':
+			case '+': 
+				// 今までvaluesCharに入っていた数字をvaluesのintに変換する
 				values[valuesCount] = atoi(valuesChar);
-				memset(valuesChar,'\0',valueCharCount);
-				if(valueCharCount != 0){
+				memset(valuesChar,'\0',valueCharCount); //初期化
+				if(valueCharCount != 0){ //もし何も入っていなかったらカウントする必要がない
 					valuesCount++;
 				}
 				valueCharCount = 0;
